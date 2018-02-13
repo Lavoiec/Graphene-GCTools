0e9cd314e43931ee41eaf7db81bc27ce71d6b6c4
Chris Lavoie <chrislavoie95@gmail.com>
2018-02-13 14:49:37 -0500

    Deleted Superfluous File

--- analytics_schema.py
+++
@@ -1,9 +0,0 @@
-import graphene
-from graphene import relay
-from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from sqlalchemy import *
-from models import db_session, Users as UsersModel, Entities as EntitiesModel, Relationships as RelationshipsModel, Groups as GroupsModel
-from models import ObjectsEntity as ObjectsEntityModel, Metadata as MetadataModel, Metastrings as MetastringsModel
-from gcga import gcga
-
-
